c6ea2c7aa3e8aa5d5f98b139d65d2d99947d3ae1
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-11-28 05:27:20 +0900

    アイコンを変更

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,11 +1,16 @@
 const { app, BrowserWindow } = require("electron");
 
+let mainWindow;
+
 function createMainWindow() {
-  const createMainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "画像圧縮ツール",
     width: 500,
     height: 600,
+    icon: `${__dirname}/assets/icons/Icon_32x32.png`,
   });
+
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
 app.on("ready", createMainWindow);
