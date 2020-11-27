36201b5e81089ba1261dbdae773a496c58aecea4
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-11-28 05:51:59 +0900

    開発環境に設定を変更

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow } = require("electron");
 
+// 開発環境に設定
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isWin = process.platform === "win32" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
 let mainWindow;
 
 function createMainWindow() {
@@ -8,9 +15,27 @@ function createMainWindow() {
     width: 500,
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_32x32.png`,
+    resizable: isDev,
   });
 
   mainWindow.loadFile(`${__dirname}/app/index.html`);
+
+  // ターミナルのエラーメッセージを非表示にするために設定
+  webPreferences: {
+    contextIsolation: true;
+  }
 }
 
 app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
